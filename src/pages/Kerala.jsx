62eef8ba0b2e236efc915e5f1f795e7d2e9852ae
Eranki Sai Vikas <saivikas@Erankis-MacBook-Pro.local>
2024-11-21 21:56:19 +0530

    Added design to product specific page

--- src/pages/Kerala.jsx
+++ src/pages/Kerala.jsx
@@ -1,4 +1,10 @@
+import { TracingBeam } from "../Components/AceternityUi/TracingBeam";
+
 function Kerala() {
-  return <div className="bg-black h-screen text-white">Kerla</div>;
+  return (
+    <TracingBeam>
+      <div className="bg-black h-screen text-white">Kerla</div>
+    </TracingBeam>
+  );
 }
 export default Kerala;
